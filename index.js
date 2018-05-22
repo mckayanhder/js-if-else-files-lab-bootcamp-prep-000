de7782b9e9331470b324ea2a4cdae47e59a59a4b
mtanhder@icloud.com <mtanhder@icloud.com>
2018-05-22 17:41:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var name = "Joe"
 var height = "74"
-else (name === "Susan") {console.log("The name is correct") }
-
+if (name === "Susan") {console.log("The name is correct") }
+else {console.log("Expected the name to be Susam") }
 
 // Don't worry about this
 module.exports = { name, height
