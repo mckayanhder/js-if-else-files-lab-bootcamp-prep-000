11be043c1e7a2ab690dd2e0bf412bb41be19aa4d
mtanhder@icloud.com <mtanhder@icloud.com>
2018-05-22 17:57:36 +0000

    automatically backed up by learn

--- tests.js
+++ tests.js
@@ -11,6 +11,6 @@ if (index.name === "Susan") {
 if (index.height === 70) {
     console.log("The height is correct")
 } else {
-    console.log("Expected: 70, Received: "+index.height)
+    console.log("Expected: 74, Received: "+index.height)
 }
 
